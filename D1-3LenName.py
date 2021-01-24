b87c1d1162c1e28e22d7aad9ac8fc4bd72bbf03c
IsraMejia <ihmaisrael@gmail.com>
2021-01-23 23:20:08 -0600

    1 day of 100, Done B)

---
+++ D1-3LenName.py
@@ -0,0 +1,12 @@
+#Write your code below this line 👇
+print('Introduce your name')
+myName = input()
+print(f"The total caracters for my name is: {len(myName)}")
+
+
+
+
+
+
+
+
---
+++ D1-Variables.py
@@ -0,0 +1,4 @@
+myName = input("What is your name? ")
+print(f"Your name is: {myName}")
+lenghtName = len(myName)
+print(f'The lenght of your name is {lenghtName}')
