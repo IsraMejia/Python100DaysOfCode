d6f94fdcc1cff3559cfabec031ef319240195ea6
IsraMejia <ihmaisrael@gmail.com>
2021-01-23 20:29:23 -0600

    First commit of my first day B)

---
+++ D1-BandName.py
@@ -0,0 +1,7 @@
+print('This code will create yout band name 🤘🏾')
+print('Whats name of the city you grew up in?')
+city = input()
+print('Whats your pets name?')
+petName = input()
+print(f'Your band name could be: {city} {petName}') 
+ 
