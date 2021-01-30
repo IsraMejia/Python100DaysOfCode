d70b22cb31d30bbd27343ea99a7dc20ca1554314
IsraMejia <ihmaisrael@gmail.com>
2021-01-29 23:52:54 -0600

    Day 6

---
+++ D6_funtions.py
@@ -0,0 +1,33 @@
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+def my_function():
+    print("\nHello, this is my_function")
+    print("Bye xd")
+
+my_function()
+
+print("uncomment the code below, and use in the next link to see my robot jaja")
+print("https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json")
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def move_turn():
+#     move()
+#     turn_left()
+
+# def jump_line():
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# 
+# for step in range(6):
+#     move_turn()
+#     jump_line()
