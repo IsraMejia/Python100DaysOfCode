39a33233adfb88ede777edb125e5a254ed139b18
IsraMejia <ihmaisrael@gmail.com>
2021-04-30 21:51:51 -0500

    Cesar cipher logic its done B)

--- D8-CesarCipher.py
+++ D8-CesarCipher.py
@@ -1,5 +1,19 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def cesar(start_text, shift_amount, cipher_direction):
+    end_text = ''
+    if cipher_direction == 'decode':
+        shift_amount *= -1 #To increment or decrement the alphabet index ;)
+        
+    for new_letter in start_text:
+        position = alphabet.index(new_letter)
+        new_index = position + shift_amount
+        end_text += alphabet[new_index]
+    
+    print(f'The {cipher_direction}d text is {end_text}')
+
+
+
 def encrypt(plain_text, shift_amount):
     cipher_text = ''
     for c in plain_text :
@@ -28,9 +42,10 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == 'encode':
-    encrypt(plain_text = text, shift_amount = shift)
-elif direction == 'decode':
-    dencrypt(plain_text = text, shift_amount = shift)
-else:
-    print('Invalid Direction') 
+# if direction == 'encode':
+#     encrypt(plain_text = text, shift_amount = shift)
+# elif direction == 'decode':
+#     dencrypt(plain_text = text, shift_amount = shift)
+# else:
+#     print('Invalid Direction') 
+cesar(start_text = text, shift_amount = shift, cipher_direction = direction)
