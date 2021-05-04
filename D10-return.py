a93fcfee6e108ce6ef1209002aa60656b6f1416a
IsraMejia <ihmaisrael@gmail.com>
2021-05-03 23:04:47 -0500

    Leap year program

---
+++ D10-return.py
@@ -0,0 +1,5 @@
+def format_name(first_name , last_name):
+    name = first_name + ' ' + last_name
+    return name.title() #title to convert in capital caracter just the first caractar per word
+
+print(format_name(first_name = 'isra', last_name = 'mejia'))
