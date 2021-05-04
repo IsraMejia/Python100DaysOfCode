10a526bc1018529711d43df90e5623a6209b57c7
IsraMejia <ihmaisrael@gmail.com>
2021-05-04 17:58:23 -0500

    docstrings

--- D10-return.py
+++ D10-return.py
@@ -1,4 +1,6 @@
 def format_name(first_name , last_name):
+    """Docstring: Take a first an last name and format it to return the title case version of the name 
+     this we can see when we put the mouse into the code function 👀"""
     name = first_name + ' ' + last_name
     return name.title() #title to convert in capital caracter just the first caractar per word
 
